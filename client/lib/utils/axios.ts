3b92f05939b8f3a8f326241d83750ff21401d15c
Faizan <syedfaizan.edu@gmail.com>
2026-02-03 16:17:32 +0530

    updated backend from api

--- client/lib/utils/axios.ts
+++ client/lib/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_BASE_URL, isProduction } from "../constants";
 
 const api = axios.create({
-    baseURL: isProduction ? "/api" : API_BASE_URL + '/api',
+    baseURL: isProduction ? "/backend" : API_BASE_URL + '/api',
     withCredentials: true,
 });
 
